6a26a25125d89aba12bed6a565dd838f69b25de5
hamadali06 <ahal06003@utb.vaxjo.se>
2023-09-15 11:26:44 +0200

    Add project files.

---
+++ uppgift 2.3 ny/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kostnad = "kostnaden för kilometer: ";
+            Console.WriteLine("Hur Många dagar ska du låna bilen?");
+            string x = Console.ReadLine();
+            int dagar = Convert.ToInt32(x);
+            Console.WriteLine("Antal kilometer som du ska köra");
+            string k = Console.ReadLine();
+            int t = Convert.ToInt32(k);
+            int km = t * 1;
+            int total = 300 + (500 * dagar) - 500 + km;
+            string pris = "Kostnaden är: ";
+            Console.WriteLine(pris + " " + total + "kr");
+
+        }
+    }
+}
